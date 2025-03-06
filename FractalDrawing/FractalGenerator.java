b62e48d6fa424ae90b81ad44350de69151b1b4b7
Trannosaurusx <etqwertyou7@gmail.com>
2025-03-06 14:59:57 -0800

    Update FractalGenerator.java

--- FractalDrawing/FractalGenerator.java
+++ FractalDrawing/FractalGenerator.java
@@ -1,4 +1,55 @@
-package FractalDrawing;
+import java.util.ArrayList;
 
-public class FractalGenerator {
+public class FractalGenerator implements FractalSubject{
+
+    private ArrayList<FractalObserver> observers;
+
+    public FractalGenerator() {
+        observers = new ArrayList<>();
+    }
+
+    /**
+     * @param observer
+     */
+    @Override
+    public void attach(FractalObserver observer) {
+        observers.add(observer);
+
+    }
+
+    /**
+     * @param observer
+     */
+    @Override
+    public void detach(FractalObserver observer) {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void notifyObservers() {
+        for (FractalObserver observer : observers) {
+            observer.update();
+        }
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public ArrayList<FractalElement> getData() {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void setOptions() {
+        notifyObservers();
+
+    }
 }
