1ce6a55c3acfd12636907f4f1b79ed81598d6be2
Trannosaurusx <etqwertyou7@gmail.com>
2025-03-06 15:02:18 -0800

    Update Main.java

--- FractalDrawing/Main.java
+++ FractalDrawing/Main.java
@@ -4,6 +4,9 @@ import src.FractalGui;
 
 public class Main {
     public static void main(String[] args){
+        FractalSubject subject = new FractalGenerator();
+        new FractalGui(subject);
+        new FractalDrawing(subject);
        try{
            src.FractalGui testGui = new FractalGui();} catch (Exception e) {
            System.out.println("Error: "+e);
