f4cc0bbb5fb851a3bb24f7be5a935e7044a666a9
liv-long-and-prosper <longolivia1129@gmail.com>
2025-03-21 22:34:16 -0700

    final touches

--- FractalDrawing/src/Circle.java
+++ FractalDrawing/src/Circle.java
@@ -3,22 +3,23 @@ package FractalDrawing.src;
 import java.awt.*;
 import java.util.Random;
 
-public record Circle(int x, int y, int radius, Color[] colors) implements FractalElement{
+public record Circle(int x, int y, int radius, Color[] colors, int colorIdx) implements FractalElement{
+    private static final Random random = new Random(42);
+
+    public Circle(int x, int y, int radius, Color[] colors){
+        int idx = 0;
+        if (colors.length > 1) {
+            idx = Math.abs(x + y) % colors.length;
+        }
+        this(x, y, radius, colors, idx);
+    }
+
     public Circle{}
 
     @Override
     public void draw(Graphics g) {
-        Color color;
-
-        if(colors.length > 1) {
-            Random random = new Random();
-            color = colors[random.nextInt(colors.length)];
-        } else {
-            color = colors[0];
-        }
-
         int diameter = radius*2;
-        g.setColor(color);
+        g.setColor(colors[colorIdx]);
         g.fillOval(x-(radius), y-(radius), diameter, diameter);
     }
 
--- FractalDrawing/src/Triangle.java
+++ FractalDrawing/src/Triangle.java
@@ -3,21 +3,22 @@ package FractalDrawing.src;
 import java.awt.*;
 import java.util.Random;
 
-public record Triangle(int[] xCoordinates, int[] yCoordinates, Color[] colors) implements FractalElement{
+public record Triangle(int[] xCoordinates, int[] yCoordinates, Color[] colors, int colorIdx) implements FractalElement{
+    private static final Random random = new Random(42);
+
+    public Triangle(int[] xCoordinates, int[] yCoordinates, Color[] colors) {
+        int idx = 0;
+        if (colors.length > 1) {
+            idx = Math.abs(xCoordinates[0] + yCoordinates[0]) % colors.length;
+        }
+        this(xCoordinates, yCoordinates, colors, idx);
+    }
+
     public Triangle{}
 
     @Override
     public void draw(Graphics g) {
-        Color color;
-
-        if(colors.length > 1) {
-            Random random = new Random();
-            color = colors[random.nextInt(colors.length)];
-        } else {
-            color = colors[0];
-        }
-
-        g.setColor(color);
+        g.setColor(colors[colorIdx]);
         g.drawPolygon(xCoordinates, yCoordinates, 3);
     }
 }
